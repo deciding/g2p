aa369adcc6d302a20796dff84c92adfc252282d7
deciding <zhangzn710@gmail.com>
2020-12-18 10:49:58 +0800

    Update g2p.py

--- g2p_en/g2p.py
+++ g2p_en/g2p.py
@@ -153,7 +153,7 @@ class G2p(object):
                        if unicodedata.category(char) != 'Mn')  # Strip accents
         text = text.lower()
         #text = re.sub("[^ a-z'.,?!\-]", "", text)
-        text = re.sub("[^ a-z'.,:?!\-]", "", text)
+        text = re.sub("[^ a-z'.,:?!，。：？！\-]", "", text)
         text = text.replace("i.e.", "that is")
         text = text.replace("e.g.", "for example")
 
